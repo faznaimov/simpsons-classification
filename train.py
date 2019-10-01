a0a119e34e66c7862662df2ff2ab0dbee1dcae3d
DZF <dzfischer@gmail.com>
2019-09-30 21:41:22 -0400

    working loaded weights

--- train.py
+++ train.py
@@ -24,6 +24,8 @@ map_characters = {0: 'abraham_grampa_simpson', 1: 'apu_nahasapeemapetilon', 2: '
         11: 'marge_simpson', 12: 'milhouse_van_houten', 13: 'moe_szyslak', 
         14: 'ned_flanders', 15: 'nelson_muntz', 16: 'principal_skinner', 17: 'sideshow_bob'}
 
+# map_characters = {0: 'abraham_grampa_simpson', 1: 'apu_nahasapeemapetilon'}        
+
 pic_size = 64
 batch_size = 32
 epochs = 200
